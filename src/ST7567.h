e2408f4e862349aeb3b89a7c03c1fd8fec6dfd71
Sarijen <109905155+Sarijen@users.noreply.github.com>
2025-03-27 20:28:30 +0100

    Update ST7567.h
    Moved LCD_WIDTH, LCD_HEIGHT and NUM_PAGES defines to ST7567.c

--- src/ST7567.h
+++ src/ST7567.h
@@ -9,15 +9,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define LCD_WIDTH 128
-#define LCD_HEIGHT 64
-#define NUM_PAGES 8
-
 typedef struct {
-  spi_inst_t* ID; // SPI instance (spi0/spi1)
+  spi_inst_t* ID;  // SPI instance (spi0/spi1)
   uint8_t MOSI;
   uint8_t SCLK;
-  uint8_t DC;
+  uint8_t DC;      // A0 in the datasheet
   uint8_t CS;
   uint8_t RST;
 } spiConfig;
@@ -44,12 +40,14 @@ void lcd_reset();
 void lcd_set_contrast(uint8_t RR_value, uint8_t EV_value);
 void lcd_enable_pwm_brightness(uint8_t pin, uint8_t pwm_frequency); // frequency in kHz
 void lcd_set_brightness(uint8_t duty_cycle); // in %
+
 void lcd_clear_screen();
 void lcd_clear_buffer();
 void lcd_display();
 void lcd_toggle_invert();
 void lcd_shift_horizontaly(uint8_t shift_amount);
 void lcd_flip(uint8_t horizontaly, uint8_t verticaly);
+
 void lcd_draw_pixel(uint8_t x, uint8_t y, uint8_t value);
 void lcd_draw_line(uint8_t x1, uint8_t y1, uint8_t x2, uint8_t y2, uint8_t value);
 void lcd_draw_rect(uint8_t x, uint8_t y, uint8_t width, uint8_t height, uint8_t value);
