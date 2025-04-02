6182aa7984a3303d8c80c4d8caef4e40b0f58570
Sarijen <sajskyv@gmail.com>
2025-04-02 09:47:14 +0200

    Added example code Removed examples/ directory from .gitignore Added examples/digital_clock.c

---
+++ examples/digital_clock.c
@@ -0,0 +1,59 @@
+#include "../src/ST7567.h"
+#include "hardware/rtc.h"
+#include "pico/util/datetime.h"
+
+// CHANGE THESE
+#define MOSI_PIN 3
+#define SCLK_PIN 2
+#define DC_PIN 5
+#define CS_PIN 6
+#define RST_PIN 7
+
+uint16_t spi_frequency = 100; // kHz
+
+char datetime_buf[256];
+char *datetime_str = &datetime_buf[0];
+
+datetime_t t = { 
+  .year = 2025,
+  .month = 4, 
+  .day = 2, 
+  .dotw = 3, // 0 is Sunday, so 5 is Friday
+  .hour = 17, 
+  .min = 54, 
+  .sec = 39 
+};
+
+char clock_string[10];
+char date_string[11];
+char day_string[10];
+
+int main() {
+  // RTC
+  rtc_init();
+  rtc_set_datetime(&t);
+  sleep_us(64);
+  datetime_to_str(datetime_str, sizeof(datetime_buf), &t);
+
+  // LCD
+  lcd_spi_init(spi0, MOSI_PIN, SCLK_PIN, DC_PIN, CS_PIN, RST_PIN, spi_frequency);
+  lcd_init();
+  lcd_clear_screen();
+
+  while (1 == 1) {
+    rtc_get_datetime(&t);
+
+    sprintf(clock_string, "%d:%d:%d", t.hour, t.min, t.sec);
+    sprintf(date_string, "%d/%d/%d", t.day, t.month, t.year);
+    sscanf(datetime_buf, "%s", day_string);
+  
+    lcd_clear_buffer();
+    
+    lcd_draw_string(1, 0, font_10x16, clock_string);
+    lcd_draw_string(1, 20, font_10x16, date_string);
+    lcd_draw_string(1, 40, font_10x16, day_string);
+
+    lcd_display();
+    sleep_ms(1000);
+  }
+}
