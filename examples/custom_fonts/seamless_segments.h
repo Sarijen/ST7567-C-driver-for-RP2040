a09b839fee380ddf8b4ef2341a89eb0f649419c0
Sarijen <sajskyv@gmail.com>
2025-04-14 21:16:17 +0200

    Added example code for implementing custom fonts font.h: Made special array END_OF_TABLE available externally, for custom fonts because its required

---
+++ examples/custom_fonts/seamless_segments.h
@@ -0,0 +1,10 @@
+#ifndef SEAMLESS_SEGMENTS_H
+#define SEAMLESS_SEGMENTS_H
+
+#include "../../../src/fonts.h"
+
+#define TABLE_SIZE_SEAMLESS 11
+
+extern font_table seamless_segments[TABLE_SIZE_SEAMLESS];
+
+#endif
