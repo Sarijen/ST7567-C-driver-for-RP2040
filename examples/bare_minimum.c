bfa2019d70384a07512f07808e9f2d8eea3ef350
Sarijen <sajskyv@gmail.com>
2025-06-30 12:47:24 +0200

    Improved bare_minimum and digital_clock examples

--- examples/bare_minimum.c
+++ examples/bare_minimum.c
@@ -10,6 +10,7 @@ int main() {
   lcd_spi_init(SPI_ID, MOSI_PIN, SCLK_PIN, DC_PIN, CS_PIN, RST_PIN, spi_frequency);
   lcd_init();
   lcd_clear_screen();
+  lcd_set_contrast(4, 21);
 
   // Smiley face :]
   lcd_fill_rect(35, 10, 10, 10, 1); // Left eye
@@ -21,6 +22,17 @@ int main() {
   lcd_draw_line(90, 30, 90, 40, 1);
 
   lcd_display(); 
+
+  // Cycle between various contrast values if the recommended isn't visible
+  sleep_ms(3500);
+  lcd_set_contrast(1, 10);
+  sleep_ms(3500);
+  lcd_set_contrast(2, 10);
+  sleep_ms(3500);
+  lcd_set_contrast(5, 10);
+  sleep_ms(3500);
+  lcd_set_contrast(6, 10);
+
 
   return 0;
 }
